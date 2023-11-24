c3f75f3759a2193471e5a31dc6e0f4fa83e675dc
Clamorinter <MRakhilov@gmail.com>
2023-11-25 00:38:12 +0300

    Applied vector to student.h

--- student.h
+++ student.h
@@ -1,33 +1,35 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include "vector.h"
 
 class Student {
 public:
 	Student();
-	Student(std::string FIO, int group, int* score);
+	Student(std::string FIO, int group, vector<int> score);
 	Student(const Student& original);
+
+	~Student();
 
 	std::string getFIO() const;
 	int getGroup() const;
-	int* getScore() const;
+	vector<int> getScore() const;
 
 	void setFIO(std::string FIO);
 	void setGroup(int group);
-	void setScore(int* score);
+	void setScore(vector<int> score);
 
 	void add_score(int grade);
 	int delete_score(int number);
-	int length_of_score() const;
 
 	friend std::ostream& operator<< (std::ostream& os, const Student& stud);
 	friend std::istream& operator>> (std::istream& is, const Student& stud);
 
 	friend Student& operator>> (Student& stud, std::string FIO);
 	friend Student& operator>> (Student& stud, int group);
-	friend Student& operator>> (Student& stud, int* score);
+	friend Student& operator>> (Student& stud, vector<int> score);
 private:
-	std::string FIO;
-	int group;
-	int* score;
+	std::string FIO = "\0";
+	int group = 0;
+	vector<int> score;
 };
